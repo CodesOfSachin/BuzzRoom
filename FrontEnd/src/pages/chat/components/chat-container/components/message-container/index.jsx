16012c18e4dcb1d239bac922f58bcbc613593e43
CodesOfSachin <codes.of.sachin@gmail.com>
2025-03-05 21:03:20 +0530

    Download Image functionality

--- FrontEnd/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ FrontEnd/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,10 +1,11 @@
 import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
 import moment from "moment";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GET_ALL_MESSAGES_ROUTES, HOST } from "/utils/constants";
 import { MdFolderZip } from 'react-icons/md';
 import { IoMdArrowDown } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 
 const MessageContainer = () => {
@@ -12,7 +13,9 @@ const MessageContainer = () => {
       const { selectedChatType, selectedChatData, userInfo, selectedChatMessages, setSelectedChatMessages,} = useAppStore();
 
 
-      
+      const [showImage, setShowImage] = useState(false);
+      const [imageURL, setImageURL] = useState(null);
+
 
 
       useEffect(() => {
@@ -98,7 +101,13 @@ const MessageContainer = () => {
             <div className={`${message.sender !== selectedChatData._id
               ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 " 
               : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}>
-                {checkIfImage(message.fileUrl) ? (<div className="cursor-pointer">
+                {checkIfImage(message.fileUrl) ? (
+                  <div className="cursor-pointer"
+                  onClick={() => {
+                    setShowImage(true);
+                    setImageURL(message.fileUrl);
+                  }}
+                  >
                   <img src={`${HOST}/${message.fileUrl}`} height={300} width={300} />
                 </div>
               ) : (
@@ -126,9 +135,35 @@ const MessageContainer = () => {
   return (
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full">
       {renderMessages()}
-      <div ref={scrollRef}></div>
+      <div ref={scrollRef}>
+        {
+          showImage && (
+            <div className="fixed z-[1000] top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center backdrop-blur-lg flex-col">
+            <div>
+              <img src={`${HOST}/${imageURL}`} 
+              className="h-[80vh] w-full bg-cover"
+              />
+            </div>
+            <div className="flex gap-5 fixed top-0 mt-5">
+              <button className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300"
+              onClick={()=> downloadFile(imageURL)}
+              >
+              < IoMdArrowDown />
+              </button>
+              <button className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300"
+              onClick={()=> {
+                setShowImage(false);
+                setImageURL(null);
+              }}
+              >
+              < IoCloseSharp />
+              </button>
+            </div>
+          </div>
+          )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MessageContainer;
