a6e6af938b58fc9547d62c73b7abf9298b3bf291
CodesOfSachin <codes.of.sachin@gmail.com>
2025-03-05 19:35:10 +0530

    Send/Receive Files Api and UI

--- FrontEnd/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ FrontEnd/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -2,7 +2,10 @@ import { apiClient } from "@/lib/api-client";
 import { useAppStore } from "@/store";
 import moment from "moment";
 import { useEffect, useRef } from "react";
-import { GET_ALL_MESSAGES_ROUTES } from "/utils/constants";
+import { GET_ALL_MESSAGES_ROUTES, HOST } from "/utils/constants";
+import { MdFolderZip } from 'react-icons/md';
+import { IoMdArrowDown } from "react-icons/io";
+
 
 const MessageContainer = () => {
       const scrollRef = useRef();
@@ -32,7 +35,13 @@ const MessageContainer = () => {
         if(scrollRef.current) {
           scrollRef.current.scrollIntoView({ behavior: "smooth"});
         }
-      }, [selectedChatMessages])
+      }, [selectedChatMessages]);
+
+
+      const checkIfImage = (filePath) => {
+        const imageRegex = /\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
+        return imageRegex.test(filePath);
+      }
 
       const renderMessages = () => {
         let lastDate = null;
@@ -56,6 +65,11 @@ const MessageContainer = () => {
       };
 
 
+      const downloadFile = (file) => {
+
+      }
+
+
       const renderDMMessages = (message) => (
        <div className={`${message.sender === selectedChatData._id 
         ? "text-left" 
@@ -67,6 +81,30 @@ const MessageContainer = () => {
               {message.content}
             </div>
          )}
+         {
+          message.messageType === "file" && (
+            <div className={`${message.sender !== selectedChatData._id
+              ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#8417ff]/50 " 
+              : "bg-[#2a2b33]/5 text-white/80 border-[#ffffff]/20"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}>
+                {checkIfImage(message.fileUrl) ? (<div className="cursor-pointer">
+                  <img src={`${HOST}/${message.fileUrl}`} height={300} width={300} />
+                </div>
+              ) : (
+              <div className="flex items-center justify-center gap-4">
+                <span className="text-white/8 text-3xl bg-black/20 rounded-full p-3">
+                < MdFolderZip />
+                </span>
+                <span>{message.fileUrl.split('/').pop()}</span>
+                <span className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300" 
+                onClick={() => downloadFile(message.fileUrl)}
+                >
+                  < IoMdArrowDown />
+                </span>
+              </div>
+              )}
+              </div>
+           )
+         }
          <div className="text-xs text-gray-600">
           {moment(message.timestamp).format("LT")}
          </div>
